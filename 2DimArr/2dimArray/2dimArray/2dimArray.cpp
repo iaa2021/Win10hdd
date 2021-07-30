e33f0c73e2d0ca5f4a71813565b1607e25834063
iaa2021 <ivanovandrey45@gmail.com>
2021-07-30 20:45:50 +0300

    2DimArray

---
+++ 2DimArr/2dimArray/2dimArray/2dimArray.cpp
@@ -0,0 +1,46 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// 2dimArray.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+    int row, column;
+    cout << "Input rows, columns: ";
+    cin >> row >> column;
+    int** array = new int* [row];
+    for (int i = 0; i < row; i++)
+        array[ i ] = new int[column];
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            array[i][j] = i * j;
+    cout << endl;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            cout << setw( 3 ) << array[i][j] << ", ";
+
+            cout << endl;
+    }
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
