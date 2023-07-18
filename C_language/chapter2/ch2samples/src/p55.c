f3cac0690d9c3c9df41000a634f7f5db08654f90
iaa2021 <ivanovandrey45@gmail.com>
2023-07-18 14:58:33 +0300

    first prog on C with adding & subtraction.

--- C_language/chapter2/ch2samples/src/p55.c
+++ C_language/chapter2/ch2samples/src/p55.c
@@ -1,7 +1,21 @@
 #include "config.h"
 #include "stdio.h"
-int main()
+/*first program on C*/
+main()
 {
-    printf("Welcome to C.\n");
+    printf("Welcome to C.\nAdding 2 integers:\n");
+    int a, b, sum;
+    printf( "Input first integer: " );
+    scanf( "%d", &a );/*cin a*/
+    printf( "Input second integer: " );
+    scanf( "%d", &b );/*cin b*/
+    sum = a + b;
+    printf( "\nSum of %d", a );
+    printf( " and %d", b );
+    printf ( " = %d", sum );
+    printf( "\nSubtraction of %d", a );
+    printf( " and %d", b );
+    printf( " = %d", a - b );
+    printf( "\n" );
     return 0;
 }
