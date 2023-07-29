5ce331bed81e8bbbddbe28b12d420a441d2b5aa5
iaa2021 <ivanovandrey45@gmail.com>
2023-07-29 14:23:03 +0300

    Chapter3 ex3.39.

---
+++ C_language/chapter3/debugUnix/src/CMakeFiles/ex3_39.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex3_39.dir/ex3_39.c.o"
+  "CMakeFiles/ex3_39.dir/ex3_39.c.o.d"
+  "ex3_39"
+  "ex3_39.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ex3_39.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
