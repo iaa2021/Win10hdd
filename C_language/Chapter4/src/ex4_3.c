bdc53a1fa08deb238fb2c0a26663c58d0b779cd7
iaa2021 <ivanovandrey45@gmail.com>
2023-08-02 19:09:22 +0300

    Chapter4 ex4.8.

--- C_language/Chapter4/src/ex4_3.c
+++ C_language/Chapter4/src/ex4_3.c
@@ -1,5 +1,6 @@
 #include "config.h"
 #include "stdio.h"
+#include "math.h"
 /*condition's sample*/
 int main()
 { 
@@ -14,6 +15,34 @@ int main()
     printf( "\nOutput of 333.547854 with width 21:\n" );
     double y = 333.547854;
     printf( "%21.6lf\n", y );
+    printf( "\npow( 2.5, 3 ) = %10.2lf\n\n", pow( 2.5, 3 ) );
+    int x = 1;
+    do
+    {
+        if( x % 5 == 0 )
+        printf( "%3d,\n", x );
+        else
+        printf( "%3d, ", x );
+
+        x++;
+    } while ( x <= 20 );
+    printf( "\n" );
+    for (int i = 1; i < 21; i++)
+    {
+        if( i % 5 == 0 )
+        printf( "%3d,\n", i );
+        else
+        printf( "%3d, ", i );
+    }
+    printf( "\n" );
+    for (float i = 0.1; i <= 1; i += 0.1 )
+    {
+        printf( "%.2f, ", i );
+    }
+    printf( "\nInput an char and integer:\n" );
+    char a = getchar(); int b;
+    scanf( "%d", &b );
+    printf( "%c, %d\n", a, b );
     printf( "\n" );
     return 0;
 }
