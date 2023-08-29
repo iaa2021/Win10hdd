f45857ebb847052161b1cb4dd8f862254089664e
iaa2021 <ivanovandrey45@gmail.com>
2023-08-29 15:52:19 +0300

    docker from windows

---
+++ Docker/bStashukLesson/python-app/main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('Welcome to my super calendar\n')
+
+year = int(input('Please, input year: '))
+month = int(input('Please, input months number: '))
+                  
+print(calendar.month( year, month ))  
+
+print('Good luck!')            
