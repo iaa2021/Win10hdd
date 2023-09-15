0f3a106fcc31d68a2ba76debde3ab8f32a4c6d93
iaa2021 <ivanovandrey45@gmail.com>
2023-09-15 17:48:06 +0300

    chapter8, complete ex8.34

--- C_language/Chapter6/src/ex8_34.c
+++ C_language/Chapter6/src/ex8_34.c
@@ -2,7 +2,7 @@
 #include "stdio.h"
 #include "string.h"
 #include "stdlib.h"
-#define SIZE 80 
+#define SIZE 800 
 #define SIZE1 40
 int main()
 { 
@@ -10,33 +10,23 @@ int main()
     printf( ".%d", PROJECT_VERSION_MINOR );
     printf( ".%d", PROJECT_VERSION_PATCH );
     printf( "\nExersise 8.34, word's frequensy in text:\n" );
-    char text[ SIZE ]; int count = 0, j = 0;
+    char text[ SIZE ]; size_t aSize = 0;
     printf( "\nInput text:\n" );
     fgets( text, SIZE, stdin );
+    int array[ 10 ] = { 0 };
     char *delim = ", ./:'";
     char *token = strtok( text, delim );
     while (token != NULL)
     {
-        count++;
+        array[ strlen( token ) - 1 ]++;
         token = strtok( NULL, delim );
     }
-    printf( "char *array's size = %d\n", count );
-    char **array = ( char ** )malloc( count * sizeof( char * ) );
-    for (int i = 0; i < count; i++)
-    {
-        char buffer[SIZE1];
-        array[ i ] = ( char *)malloc( strlen( buffer ) * sizeof(char) );
-    }
     
-    char *token1 = strtok( text, delim );
-    while (token1 != NULL)
+    printf( "\nWord's frequensy in text:\n" );
+    for (int i = 0; i < 10; i++)
     {
-        strcpy( array[ j ], token1);
-        j++;
-        printf( "%s, ", array[ j ] );
-        token1 = strtok( NULL, delim );
+        printf( "word with %d letters: %d\n", i + 1, array[i] );  
     }
-    
     printf( "\n" );
     return 0;
 }
