0f46b128006485024a2b8e9f39fe2ae79383a079
iaa2021 <ivanovandrey45@gmail.com>
2023-09-19 20:05:41 +0300

    chapter8,  ex8.38 int via words

--- C_language/Chapter6/src/ex8_38.c
+++ C_language/Chapter6/src/ex8_38.c
@@ -3,32 +3,33 @@
 #include "string.h"
 #define SIZE 80 
 #define SIZE1 40
-int rank( int, int );
 int main()
 { 
     printf( "Project version is %d", (PROJECT_VERSION_MAJOR) );
     printf( ".%d", PROJECT_VERSION_MINOR );
     printf( ".%d", PROJECT_VERSION_PATCH );
     printf( "\nExersise 8.38, numbers via words:\n" );
-    char *array[3][10] = {
+    char *array[3][9] = {
         { "one", "two", "three", "four", "five", "six", "seven", \
-        "eight", "nine", "ten" },
+        "eight", "nine" },
         { "eleven", "twelve", "thirteen", "forteen", "fifteen", \
-            "sixteen", "seventeen", "eighteen", "nineteen", "hundreed"},
-        { "twenty", "thirty", "forty", "fifty", "sixty", \
-        "seventy", "eighty", "ninety", "thousand", "million" }
+            "sixteen", "seventeen", "eighteen", "nineteen"},
+        { "ten","twenty", "thirty", "forty", "fifty", "sixty", \
+        "seventy", "eighty", "ninety" }
     };
     int number, j;
     printf( "\nInput number:\n" );
     scanf( "%d", &number );
     int numbers[] = { 1000000, 1000, 100, 10 };
+    char *ranks[] = {"million", "thousand", "hundreed", "ten"};
+    for (size_t i = 0; i < 4; i++)
+    {
+        if( number / numbers[i] > 0 )
+            printf( "%s of %s ", array[number / numbers[i]], ranks[i] );
+
+        number %= numbers[i];
+    }
     
     printf( "\n" );
     return 0;
-}
-int rank( int number, int deleter )
-{
-    
-    for (int i = 0; i < sizeof( numbers )/sizeof( int ); i++)
-    return  number / numbers[ i ];
 }
