1032fd98602b7d8ee99e5db4849a21fae09ecef6
iaa2021 <ivanovandrey45@gmail.com>
2023-09-29 12:31:19 +0300

    chapter9 p429 continue

--- C_language/Chapter9FormattedInputOutput/src/p427.c
+++ C_language/Chapter9FormattedInputOutput/src/p427.c
@@ -19,7 +19,26 @@ int main()
     x = 873; double y1 = 123.65476;
     char *string = "\nHappy birthday.\n";
     printf( "Using presision for integers:\n" );
-    printf( "\t%.4d\n\t%.9d\n", x, x );//with . output with zero before number
+    printf( "\t%.4d\n\t%.9d\n", x, x );/*with . output with zero before number*/
+    printf( "Finding out the length of string via %%n specificator:\n" );
+    char *text = "Hello world again"; int a;
+    printf( "%s%n", text, &a );
+    printf( "\nThe text's length = %d\n", a );
+    printf( "\nThe text's length in 8'format = %#o\n", a );
+    printf( "\nThe text's length in 16'format = %#x\n", a );
+    char text1[100];
+    printf( "\nInput text:\n" );
+    fgets( text1, 100, stdin );
+    puts(text1);
+    int n[10];
+    printf( "\nInput less or equal 10 integers:\n" );
+    scanf( "%[^a]", n );
+    printf( "\nArray is:\n" );
+    for (size_t i = 0; i < 10; i++)
+    {
+        printf( "%d, ", n[i] );
+    }
+    
     printf( "\n" );
     return 0;
 }
