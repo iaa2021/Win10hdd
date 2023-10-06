32b2e21e8f1c8cb543105cbcbbaef1d38283e0f1
iaa2021 <ivanovandrey45@gmail.com>
2023-10-06 19:22:05 +0300

    chapter10 p473

---
+++ C_language/Chapter9FormattedInputOutput/debugUnix/src/CMakeFiles/p473.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p473.dir/p473.c.o"
+  "CMakeFiles/p473.dir/p473.c.o.d"
+  "p473"
+  "p473.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/p473.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
