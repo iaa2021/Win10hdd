9a9d440d74df1fc266c73a1b9e980aab0301b2d3
iaa2021 <ivanovandrey45@gmail.com>
2023-10-30 17:11:18 +0200

    chapter11 - ex11.7, working

--- C_language/Chapter11Files/src/ex11_3.c
+++ C_language/Chapter11Files/src/ex11_3.c
@@ -7,7 +7,7 @@ int main()
     printf( ".%d", PROJECT_VERSION_MINOR );
     printf( ".%d", PROJECT_VERSION_PATCH );
     printf( "\nExersise 11.3.\n" );
-    FILE *ofPtr; 
+    /*FILE *ofPtr; 
     if((ofPtr = fopen("oldmast.txt", "r+")) == NULL)
     printf("File oldmast.txt cannot be opened.\n");
     FILE *tfPtr;
@@ -17,18 +17,27 @@ int main()
     if((ofPtr = fopen("newmast.txt", "w")) == NULL)
     printf("File oldmast.txt cannot be opened.\n");
     int accountNumber; char name[15]; double balance, amount;
-    /*fscanf(ofPtr, "\n%-6d%-15s%-7.2lf\n", &accountNumber, name, &balance);
+    fscanf(ofPtr, "\n%-6d%-15s%-7.2lf\n", &accountNumber, name, &balance);
     fscanf(tfPtr, "%-d%-7.2lf", &accountNumber, &amount);
     fprintf(nfPtr, "%d%s%lf", accountNumber, name, balance);*/
-    fprintf(tfPtr, "%-10s%-12s", "Account", "Transaction");
-    printf("\nInput acc's number, transaction, EOF to end input:\n? ");
-    scanf( "%d%lf", &accountNumber, &amount );
-    while(!feof(stdin))
-    {
-        fprintf(tfPtr, "%-10d%-12.2lf", accountNumber, amount);
-        printf("\nInput acc's number, transaction, EOF to end input:\n? ");
+    printf( "\nExersise 11.8, creating transaction and client's lists.\n" );
+    int accountNumber; char name[15]; double balance, amount;
+    FILE *tfPtr;
+    if((tfPtr = fopen("trans.txt", "w+")) == NULL)
+    printf("File trans.txt cannot be opened.\n");
+    else{
+        fprintf(tfPtr,"%-9s%-12s", "Account", "Transaction");
+        printf("\nInput acc's number & transaction's sum, EOF to end input:\n");
         scanf( "%d%lf", &accountNumber, &amount );
-    }
+        }
+        while( !feof(stdin) )
+        {
+            fprintf( tfPtr, "\n%-9d%-12.2lf\n", accountNumber, amount );
+            printf("\nInput acc's number & transaction's sum,\
+            EOF to end input:\n? ");
+            scanf( "%d%lf", &accountNumber, &amount );
+        }
     fclose(tfPtr);
+    printf( "\n" );
     return 0;
 }
