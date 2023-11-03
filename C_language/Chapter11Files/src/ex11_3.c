2764e04a70ea1e8c6c8632d6580cec7c55df8e30
iaa2021 <ivanovandrey45@gmail.com>
2023-11-03 18:08:07 +0200

    ex11.7 3.11.23

--- C_language/Chapter11Files/src/ex11_3.c
+++ C_language/Chapter11Files/src/ex11_3.c
@@ -1,43 +1,73 @@
 #include "config.h"
 #include "stdio.h"
-
+void trans(FILE *);
+void oldmast(FILE *);
 int main()
 {
     printf( "Project version is %d", (PROJECT_VERSION_MAJOR) );
     printf( ".%d", PROJECT_VERSION_MINOR );
     printf( ".%d", PROJECT_VERSION_PATCH );
-    printf( "\nExersise 11.3.\n" );
-    /*FILE *ofPtr; 
-    if((ofPtr = fopen("oldmast.txt", "r+")) == NULL)
-    printf("File oldmast.txt cannot be opened.\n");
-    FILE *tfPtr;
-    if((tfPtr = fopen("trans.txt", "w")) == NULL)
-    printf("File trans.txt cannot be opened.\n");
-    FILE *nfPtr;
-    if((ofPtr = fopen("newmast.txt", "w")) == NULL)
-    printf("File oldmast.txt cannot be opened.\n");
-    int accountNumber; char name[15]; double balance, amount;
-    fscanf(ofPtr, "\n%-6d%-15s%-7.2lf\n", &accountNumber, name, &balance);
-    fscanf(tfPtr, "%-d%-7.2lf", &accountNumber, &amount);
-    fprintf(nfPtr, "%d%s%lf", accountNumber, name, balance);*/
+    printf( "\nExersise 11.3, creation oldmast.txt, trans.txt.\n" );
     printf( "\nExersise 11.8, creating transaction and client's lists.\n" );
-    int accountNumber; char name[15]; double balance, amount;
-    FILE *tfPtr;
-    if((tfPtr = fopen("trans.txt", "r+")) == NULL)
-    printf("File trans.txt cannot be opened.\n");
-    else{
-        fprintf(tfPtr,"%-9s%-12s", "Account", "Transaction");
-        printf("\nInput acc's number & transaction's sum, EOF to end input:\n");
-        scanf( "%d%lf", &accountNumber, &amount );
+    int accountNumber, choice; char name[20]; double balance, amount;
+    FILE *tfPtr; FILE *ofPtr;
+    printf( "Input your choice, 0 to end input:\n" );
+    printf( "1 - create trans.txt;\n2 - create oldmast.txt;\n" );
+    scanf("%d", &choice);
+    while( choice != 0 )
+    {
+        switch(choice){
+            case 1:
+            trans(tfPtr);
+            break;
+            case 2:
+            oldmast(ofPtr);
+            break;
+            default:
+            printf("\nYou've entered wrong choice.\n");
         }
-        while( !feof(stdin) )
-        {
-            fprintf( tfPtr, "\n%-9d%-12.2lf\n", accountNumber, amount );
-            printf("\nInput acc's number & transaction's sum,\
-            EOF to end input:\n? ");
-            scanf( "%d%lf", &accountNumber, &amount );
-        }
-    fclose(tfPtr);
+        printf( "Input your choice, 0 to end input:\n" );
+        printf( "1 - create trans.txt;\n2 - create oldmast.txt;\n" );
+        scanf("%d", &choice);
+    }
     printf( "\n" );
     return 0;
+}
+void trans(FILE *tfPtr)
+{
+    if((tfPtr = fopen("trans.txt", "r+")) == NULL)
+    printf( "\nFile trans.txt cannot be opened.\n" );
+    else
+    {
+        int account; double transaction;
+        fprintf(tfPtr, "%-10s%-15s", "Account", "Transaction");
+        printf("\nInput account, sum of transaction, EOF to end input:\n");
+        scanf("%d%lf", &account, &transaction);
+        while(!feof(stdin))
+        {
+            fprintf(tfPtr, "%-10d%-15lf\n", account, transaction);
+            printf("\nInput account, sum of transaction, EOF to end input:\n");
+            scanf("%d%lf", &account, &transaction);
+        }
+    }
+    fclose(tfPtr);
+}
+void oldmast(FILE *ofPtr)
+{
+    if((ofPtr = fopen("oldmast.txt", "r+")) == NULL)
+    printf( "\nFile oldmast.txt cannot be opened.\n" );
+    else
+    {
+        int account; char name[20];double balance;
+        fprintf(ofPtr, "%-10s%-20s%-15s", "Account", "Name", "Balance");
+        printf("\nInput account, name, sum of balance; EOF to end input:\n");
+        scanf("%d%s%lf", &account, name, &balance);
+        while(!feof(stdin))
+        {
+            fprintf(ofPtr, "%-10d%-20s%-15lf", account, name, balance);
+            printf("\nInput account, name, sum of balance; EOF to end input:\n");
+            scanf("%d%s%lf", &account, name, &balance);
+        }
+    }
+    fclose(ofPtr);
 }
