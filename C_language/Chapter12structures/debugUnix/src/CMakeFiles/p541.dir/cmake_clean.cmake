d96d5353345c4308ca1242a8e6bcc664e8d9cdd3
iaa2021 <ivanovandrey45@gmail.com>
2023-11-18 16:16:48 +0200

    add ch12 p541

---
+++ C_language/Chapter12structures/debugUnix/src/CMakeFiles/p541.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p541.dir/p541.c.o"
+  "CMakeFiles/p541.dir/p541.c.o.d"
+  "p541"
+  "p541.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/p541.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- C_language/Chapter12structures/makeRun/runR.sh
+++ C_language/Chapter12structures/makeRun/runR.sh
@@ -1,4 +1,4 @@
 echo [*] running release configuration with Ninja
 cd ../releaseNinja
-src/ex11_13
+src/p541
 cd ..
