7d0ae480c8812305cd08ab69f64ac64bde6fa736
iaa2021 <ivanovandrey45@gmail.com>
2023-12-01 19:59:35 +0200

    ex12.4 done

--- C_language/Chapter12structures/src/ex12_4.c
+++ C_language/Chapter12structures/src/ex12_4.c
@@ -11,6 +11,8 @@ typedef struct gradeNode GRADENODE;
 typedef struct gradeNode* GRADENODEPTR;
 void insert(GRADENODEPTR *, double, char *);
 void print(GRADENODEPTR);
+char *delete(GRADENODEPTR *, char *);
+int isEmpty(GRADENODEPTR);
 int main()
 {
     printf( "Project version is %d", (PROJECT_VERSION_MAJOR) );
@@ -18,7 +20,7 @@ int main()
     printf( ".%d", PROJECT_VERSION_PATCH );
     printf( "\nEx 12.4, linked list.\n" );
     int choice; double value; char lname[20];
-    printf("\nInput choice, 1 - to insert, 0 to end input: ");
+    printf("\nInput choice, 1 - to insert, 3 - print, 2 - to delete, 0 to end input: ");
     scanf("%d", &choice);
     while(choice != 0){
         switch(choice){
@@ -28,8 +30,21 @@ int main()
             scanf("%lf", &value);
             printf("\nInput last name: ");
             /*fgets(lname, sizeof(lname), stdin);*/
+            scanf("%20s", lname);
             insert(&start, value, lname);
             }
+            break;
+            case 2:
+            if(!isEmpty(start)){
+                printf("\nInput last name, which position to be deleted:\n");
+                scanf("%20s", lname);
+                if(delete(&start, lname))
+                printf("\n%s deleted.\n", lname);
+                else
+                printf("\n%s wasn't found.\n", lname);
+            }
+            else
+            printf("\nThe list is empty.\n");
             break;
             case 3:
             print(start);
@@ -38,7 +53,7 @@ int main()
             printf("\nYou've entered wrong choice.\n");
             break;
         }
-        printf("\nInput choice, 1 - insert, 3 - print, 0 to end input: ");
+        printf("\nInput choice, 1 - insert, 2 - to delete, 3 - print, 0 to end input: ");
         scanf("%d", &choice);
     }
     return 0;
@@ -71,7 +86,33 @@ void insert(GRADENODEPTR *start, double value, char *text){
 }
 void print(GRADENODEPTR start){
     while(start != NULL){
-        printf("%s, %lf\n", start ->last, start ->grade);
+        printf("%-10s%4.2lf\n", start ->last, start ->grade);
         start = start ->next;
     }
+}
+char *delete(GRADENODEPTR *start, char *name){
+    GRADENODEPTR current, previous, temp;
+        if(strcmp((*start) ->last, name) == 0){
+            temp = *start;
+            *start = (*start) ->next;
+            free(temp);
+            return name;
+            }
+        else{
+            current = (*start) ->next; previous = *start;
+            while(current != NULL && strcmp(current -> last, name) != 0){
+            previous = current;
+            current = current ->next;
+            }
+            if(current != NULL){
+                temp = current;
+                previous ->next = current ->next;
+                free(temp);
+                return name;
+            }
+        }
+    return '\0';
+}
+int isEmpty(GRADENODEPTR start){
+    return start == NULL;
 }
