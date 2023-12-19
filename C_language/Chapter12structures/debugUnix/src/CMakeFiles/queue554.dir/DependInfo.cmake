bdd72dd7ac3062b5d32083f70eb8f6dcdf51371b
iaa2021 <ivanovandrey45@gmail.com>
2023-12-19 21:24:48 +0300

    ex12.22 in ubuntu

--- C_language/Chapter12structures/debugUnix/src/CMakeFiles/queue554.dir/DependInfo.cmake
+++ C_language/Chapter12structures/debugUnix/src/CMakeFiles/queue554.dir/DependInfo.cmake
@@ -11,8 +11,8 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/iaa/Desktop/gitStore/Win10hdd/C_language/Chapter12structures/src/queue554.c" "src/CMakeFiles/queue554.dir/queue554.c.o" "gcc" "src/CMakeFiles/queue554.dir/queue554.c.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
