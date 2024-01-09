8cfe4d6c2f13db189731218020529ab3e6fb3630
iaa2021 <ivanovandrey45@gmail.com>
2024-01-09 19:18:57 +0300

    work with ObjectiveC

--- Objective_C/S_G_Kochan/p13.m
+++
@@ -1,8 +0,0 @@
-#import <Foundaiton/Foundation.h>
-
-int main(int argc, char *argv[]){
-    @autoreleasepool{
-        NSLog(@"Programming is fun.");
-    }
-    return 0;
-}
---
+++ Objective_C/S_G_Kochan/src/p13.m
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, char *argv[]){
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    pool {
+        NSLog (@"Programming is fun.");
+    }
+    return 0;
+}
