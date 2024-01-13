8c16cb9a3740cdaa00abb12d99b25d9abe0bd390
iaa2021 <ivanovandrey45@gmail.com>
2024-01-13 14:13:01 +0200

    ex14.8, Datel, continue

--- C_language/Chapter14Questions/src/ex14_8.c
+++ C_language/Chapter14Questions/src/ex14_8.c
@@ -9,27 +9,76 @@ int main( int argc, char *argv[]){
     printf("\nExercise 14.8, record from one file to another in reverse order.\n");
     FILE *out, *in; char c; int count = 0;
     if((out = fopen(argv[1], "r")) != NULL){
-        if((out = fopen(argv[2], "r+")) != NULL){
+        if((in = fopen(argv[2], "w")) != NULL){
+            // Find the size of the file
+            fseek(out, 0, SEEK_END);
+            size_t fileSize = ftell(out);
+            rewind(out);
+            printf("\nSize of char *text via chat GPT is %d.\n", fileSize);
             while((c = getc(out)) != EOF){
                 putchar(c);/*file output and finding out it's size*/
                 count++;
             }
             printf("\nSize of char *text is %d.\n", count);
-            char *text = malloc(count * sizeof(char));
-            printf("\nCreate char * text.\n");
-            count = 0;
-            while((c = getc(out)) != EOF){
-               text[count] = c;
-               count++;
-            }
+            printf("\nCreate reverse file.\n");
+
             printf("\nChar text is:\n");
-            for (int i = 0; i < count; i++)
-            {
-                printf("%c", text[i]);
-            }
+            
             
         }
     }
     printf("\nEnd of run.\n");
     return 0;
 }
+/*#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 1000
+
+int main() {
+    FILE *inputFile, *outputFile;
+    char content[MAX_SIZE];
+    long fileSize;
+
+    // Open the input file in read mode
+    inputFile = fopen("input.txt", "r");
+
+    if (inputFile == NULL) {
+        perror("Error opening input.txt");
+        return 1;
+    }
+
+    // Open the output file in write mode
+    outputFile = fopen("output.txt", "w");
+
+    if (outputFile == NULL) {
+        perror("Error opening output.txt");
+        fclose(inputFile);
+        return 1;
+    }
+
+    // Find the size of the file
+    fseek(inputFile, 0, SEEK_END);
+    fileSize = ftell(inputFile);
+    rewind(inputFile);
+
+    // Read the content from the file
+    if (fileSize < MAX_SIZE) {
+        fread(content, sizeof(char), fileSize, inputFile);
+
+        // Write the content in reverse order to the output file
+        for (long i = fileSize - 1; i >= 0; i--) {
+            fputc(content[i], outputFile);
+        }
+
+        printf("Content reversed and written to output.txt successfully.\n");
+    } else {
+        printf("File size exceeds the maximum buffer size.\n");
+    }
+
+    // Close the files
+    fclose(inputFile);
+    fclose(outputFile);
+
+    return 0;
+}*/
